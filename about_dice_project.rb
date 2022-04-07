e104c8efb3cca72e7ed64d188f21985b03918d79
Jeroen Mols <jeroen@jeroenmols.com>
2022-04-07 13:46:25 +0200

    fix dice

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+
+  attr_accessor :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(times_to_roll)
+    @values = []
+    times_to_roll.times {
+      @values << rand(5)+1
+    }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
